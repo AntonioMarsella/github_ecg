ff712a5bd84647affddb37d1cbb4780427b689f9
DigitalDigger <michael.glazunoff@gmail.com>
2018-12-12 18:38:57 +0100

    No alpha channel -> jpgs

--- wavelet_transform.py
+++ wavelet_transform.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 import pywt
 import wfdb
 
+import os.path
+
+
+
 time, sst = pywt.data.nino()
 
 db_path = '/home/michael/Projects/ECGSleepApnea/www.physionet.org/physiobank/database/apnea-ecg/'
@@ -12,12 +16,12 @@ db_path = '/home/michael/Projects/ECGSleepApnea/www.physionet.org/physiobank/dat
 DATA_A_SIZE = 20
 DATA_B_SIZE = 5
 DATA_C_SIZE = 10
+DATA_X_SIZE = 35
 CHUNK_SIZE = 6000
 
 patient_groups = {'a' : DATA_A_SIZE, 'b' : DATA_B_SIZE, 'c' : DATA_C_SIZE}
 
 for k in patient_groups:
-
     for i in range(1, patient_groups[k]+1):
         filename = str(k + '%02d' % i)
         full_path_to_filename = str(db_path + filename)
@@ -33,32 +37,41 @@ for k in patient_groups:
                 current_chunk_size=record_len-last_chunk_index
                 sampto_index= j + current_chunk_size
 
-
-            sst, fields = wfdb.rdsamp(full_path_to_filename, sampfrom = j, sampto = sampto_index)
-            sst = sst.flatten()
-
-            dt = 0.01
-
-            # Taken from http://nicolasfauchereau.github.io/climatecode/posts/wavelet-analysis-in-python/
-            wavelet = 'cmor1.5-1.0'
-            #wavelet = 'morl'
-            scales = np.arange(1, 125)
-
-            [cfs, frequencies] = pywt.cwt(sst, scales, wavelet)
-
-            #plt.imshow(cfs, cmap='PRGn', aspect='auto', vmax=abs(cfs).max(), vmin=-abs(cfs).max())
-            #plt.show()
-
-            power = (abs(cfs)) ** 2
-            plt.ioff()
-
-            f, ax = plt.subplots(figsize=(15, 10))
-            time = range(0, current_chunk_size)
-            ax.contourf(time, np.log2(frequencies), power)
-
             if j // CHUNK_SIZE < ann.ann_len:
-                ann_i = j // CHUNK_SIZE
+                    ann_i = j // CHUNK_SIZE
 
-            f.savefig('/home/michael/Projects/ECGSleepApnea/cwt_classes/' + ann.symbol[ann_i] + '/' + filename + '_' + str(j) + '.png')
-            plt.close(f)
+            fname = '/home/michael/Projects/ECGSleepApnea/cwt_classes_jpgs/' + ann.symbol[ann_i] + '/' + filename + '_' + str(j) + '.jpg'
+
+            if not os.path.isfile(fname):
+                sst, fields = wfdb.rdsamp(full_path_to_filename, sampfrom = j, sampto = sampto_index)
+                sst = sst.flatten()
+
+                dt = 0.01
+
+                wavelet = 'cmor1.5-1.0'
+                scales = np.arange(1, 125)
+                frequencies = pywt.scale2frequency(wavelet, scales) / dt
+
+
+                [cfs, frequencies] = pywt.cwt(sst, scales, wavelet)
+                frequencies = pywt.scale2frequency(wavelet, scales) / dt
+
+                #plt.imshow(cfs, cmap='PRGn', aspect='auto', vmax=abs(cfs).max(), vmin=-abs(cfs).max())
+                #plt.show()
+
+                power = (abs(cfs)) ** 2
+                plt.ioff()
+
+                f, ax = plt.subplots(figsize=(15, 10))
+                time = range(0, current_chunk_size)
+                ax.contourf(time, frequencies, power)
+
+                # f.patch.set_visible(False)
+                # ax.axis('off')
+
+
+
+                name = '/home/michael/Projects/ECGSleepApnea/cwt_classes_jpgs/' + ann.symbol[ann_i] + '/' + filename + '_' + str(j) + '.jpg'
+                f.savefig(fname)
+                plt.close(f)
 
